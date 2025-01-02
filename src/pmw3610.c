0511e0dfdbda30bf78f17a904149aa6036a59031
H1r0k1kun <hirokifk6@gmail.com>
2025-01-02 21:52:46 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -633,9 +633,9 @@ static int pmw3610_report_data(const struct device *dev) {
 #ifdef CONFIG_PMW3610_ADJUSTABLE_MOUSESPEED
     int16_t movement_size = abs(raw_x) + abs(raw_y);
 
-    float speed_multiplier = 1.3; //速度の倍率
+    float speed_multiplier = 1.0; //速度の倍率
     if (movement_size > 60) {
-        speed_multiplier = 6.0;
+        speed_multiplier = 5.0;
     }else if (movement_size > 30) {
         speed_multiplier = 4.0;
     }else if (movement_size > 5) {
@@ -647,7 +647,7 @@ static int pmw3610_report_data(const struct device *dev) {
     }else if (movement_size > 2) {
         speed_multiplier = 1.0;
     }else if (movement_size > 1) {
-        speed_multiplier = 0.5;
+        speed_multiplier = 0.8;
     }
 
     raw_x = raw_x * speed_multiplier;
