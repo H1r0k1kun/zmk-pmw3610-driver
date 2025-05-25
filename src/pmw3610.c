fed14f4cc8976aba697508b20dcabef5e6da8a61
H1r0k1kun <hirokifk6@gmail.com>
2025-05-25 23:06:10 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -747,7 +747,7 @@ static int pmw3610_report_data(const struct device *dev) {
     if (IS_ENABLED(CONFIG_PMW3610_INVERT_Y)) {
         y = -y;
     }
-    //オートマウス鈍くする
+    
     #if AUTOMOUSE_LAYER > 0
     if (input_mode == MOVE && (abs(x)+abs(y) > 0.6) &&
             (automouse_triggered || zmk_keymap_highest_layer_active() != AUTOMOUSE_LAYER)
